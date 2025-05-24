027ea6cf30cc12e106b7d648af0739bfd1a6d896
Ahmad Hilman Dani <ahmadhilmanlagi@gmail.com>
2025-05-24 21:52:02 +0700

    update: make a omponents whith a prop so that is reusable

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,28 +1,7 @@
-import Button from "./Button";
-import Input from "./Input";
-import LogoLight from "../assets/logo/logo-mental-butler-dark.png"
-import LogoDark from "../assets/logo/logo-mental-butler-light.png"
-
-export default function Card() {
+export default function Card({ children }) {
   return (
     <div class="w-full max-w-md p-4 bg-white border border-gray-200 rounded-xl sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700">
-      <form class="space-y-6" action="#">
-        <img src={LogoLight} alt="" className="w-36 mx-auto" />
-        <div>
-          <Input></Input>
-        </div>
-        <div>
-          <Input></Input>
-        </div>
-        <div class="flex items-start">
-          <a href="#" class="ms-auto text-sm">Lost Password?</a>
-        </div>
-        <Button buttonType="primary" isExtend={true}>Login</Button>
-        <div class="text-sm font-medium text-gray-500 dark:text-gray-300">
-          Not registered? <a href="#">Create account</a>
-        </div>
-      </form>
+      {children}
     </div>
-
   )
 }
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,10 +1,8 @@
-export default function Input() {
+export default function Input({ labelProp, placeholderProp, typeProp, inputId, onChangeProp }) {
   return (
     <>
-      <div>
-        <label for="small-input" class="block mb-2 text-sm font-medium text-dark dark:text-light">Email</label>
-        <input placeholder="ahmadhilmanlagi@gmail" type="text" id="small-input" class="block w-full p-2 text-dark rounded-lg bg-gray-50 outline outline-gray-300 text-sm  focus:outline-primary-500  dark:bg-gray-700 dark:outline-gray-600 dark:placeholder-gray-400 dark:text-light transition-all" />
-      </div>
+      <label for={inputId} class="block mb-2 text-sm font-medium text-dark dark:text-light">{labelProp}</label>
+      <input onChange={onChangeProp} placeholder={placeholderProp} type={typeProp} id={inputId} class="block w-full p-2 text-dark rounded-lg bg-gray-50 outline outline-gray-300 text-sm focus:outline-primary-500  dark:bg-gray-700 dark:outline-gray-600 dark:placeholder-gray-400 dark:text-light transition-all" />
     </>
   )
 }
